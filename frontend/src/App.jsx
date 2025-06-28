0e1d5b18c94a72d94d76cac1e3cbb3ec8c81725c
HrutvikKhatkar <hrutvikkhatkar25@gmail.com>
2025-06-29 01:22:27 +0530

    Initial commit - React + Tailwind setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><div className="text-red-600 text-4xl font-bold underline">Tailwind Works!</div>
+</div>
+    
+  )
+}
+
+export default App
