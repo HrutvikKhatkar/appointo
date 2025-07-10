b918ad3ab11d1111f1477cd5ca6d6ff600b10571
Hrutvik Khatkar <147256332+HrutvikKhatkar@users.noreply.github.com>
2025-07-10 22:23:45 +0530

    Update Footer.jsx

--- updatedFrontend/src/components/Footer.jsx
+++ updatedFrontend/src/components/Footer.jsx
@@ -24,8 +24,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>+91 8308458824</li>
+            <li>hrutvikkhatkar25@gmail.com</li>
           </ul>
         </div>
 
@@ -33,7 +33,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ Appointo.com - All Right Reserved.</p>
       </div>
 
     </div>
